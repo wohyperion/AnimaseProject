6661e9cdb09843ed1809012ffd46015836f9a2a1
wohyperion <whisperofhyperion@gmail.com>
2019-06-12 02:29:12 +0700

    Update model and make migrations

--- movies/models.py
+++ movies/models.py
@@ -1,18 +1,45 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Genre(models.Model):
-    pass
+    """
+    Describe genre for movie
+    """
+
+    # Fields
+    title = models.CharField(max_length=100, help_text='Enter genre title (Action, Horror, etc.)')
+    description = models.TextField(max_length=500, null=True, blank=True,
+                                   help_text='Enter description of genre or leave it blank')
+
+    # Methods
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('movies:genre-detail', kwargs={'pk': self.pk})
 
 
 class Studio(models.Model):
-    pass
+    """
+    Represent studio
+    """
+
+    # Fields
+    name = models.CharField(max_length=100, help_text='Enter studio name')
+
+    # Methods
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('movies:studio-detail', kwargs={'pk': self.pk})
 
 
 class Movie(models.Model):
     """
-    Represent anime
+    Represent movie item (anime)
     """
 
     # Constants
@@ -48,7 +75,7 @@ class Movie(models.Model):
     type = models.CharField(max_length=10, choices=TYPES,
                             help_text='Choice a type of anime (TV, Movie, OVA or Special)')
     # TODO: Movie.episodes check episodes > 0
-    episodes = models.PositiveSmallIntegerField(help_text='Enter a number of episodes')
+    episodes = models.PositiveSmallIntegerField(help_text='Enter a number of episodes', default=12)
     status = models.CharField(max_length=15, choices=STATUSES,
                               help_text='Choice the current status of (e.g. Completed or not, announced)')
     release_date = models.DateField(null=True, blank=True, help_text='Date of announce or first episode release')
@@ -58,29 +85,78 @@ class Movie(models.Model):
     source = models.CharField(max_length=10, choices=SOURCES,
                               help_text='The type of resource that became the basis of current anime')
     # TODO: Movie.duration check duration > 0
-    duration = models.PositiveSmallIntegerField(help_text='Duration of episode in minutes')
-    rating = models.CharField(max_length=30, choices=RATINGS, help_text='Choice MPAA film rating')
+    duration = models.PositiveSmallIntegerField(help_text='Duration of episode in minutes', default=23)
+    rating = models.CharField(max_length=30, choices=RATINGS, help_text='Choice MPAA film rating', default='R')
     studio = models.ForeignKey(Studio, on_delete=models.SET_NULL, null=True, blank=True,
                                help_text='Select the studio that released that anime')
     genre = models.ManyToManyField(Genre, help_text='Select the genres')
-    summary = models.TextField(max_length=1000, help_text='Enter a summary')
+    summary = models.TextField(max_length=1000, help_text='Enter a summary', null=True, blank=True)
     # TODO: Movie.cover_url check resource response before save
-    cover_url = models.URLField(help_text='Enter a cover URL address')
+    cover_url = models.URLField(help_text='Enter a cover URL address', blank=True, null=True)
     # TODO: Movie.trailer_url check youtube.com domain
-    trailer_url = models.URLField(help_text='Enter a YouTube video trailer URL')
+    trailer_url = models.URLField(help_text='Enter a YouTube video trailer URL', null=True, blank=True)
+
+    # Methods
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('movies:movie-detail', kwargs={'pk': self.pk})
 
 
 class Score(models.Model):
-    pass
+    """
+    User can set a score and leave a comment
+    """
 
+    # Fields
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # TODO: Score.value check: 0 < value <= 10
+    value = models.PositiveSmallIntegerField(help_text='Enter your score')
+    message = models.TextField(max_length=500, help_text='Justify your opinion', null=True, blank=True)
 
-class Comment(models.Model):
-    pass
+    # Methods
+    def __str__(self):
+        # TODO: make readable str representation
+        return self.movie.title
+
+    def get_absolute_url(self):
+        return reverse('movies:score-detail', kwargs={'pk': self.pk})
 
 
 class Favorite(models.Model):
-    pass
+    """
+    User can add liked movie to favorites list
+    """
+
+    # Fields
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    # TODO: try ManyToManyField
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Methods
+    def __str__(self):
+        # TODO: make readable str representation
+        return self.movie.title
+
+    def get_absolute_url(self):
+        return reverse('movies:favorite-detail', kwargs={'pk': self.pk})
 
 
 class Related(models.Model):
-    pass
+    """
+    Show relation between movies
+    """
+
+    # Fields
+    main_movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='main_movie')
+    # TODO: check main_movie != relate_movies
+    related_movies = models.ManyToManyField(Movie, related_name='related_movies')
+
+    # Methods
+    def __str__(self):
+        return self.main_movie.title
+
+    def get_absolute_url(self):
+        return reverse('movies:related-detail', kwargs={'pk': self.pk})
