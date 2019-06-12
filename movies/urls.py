f65874d5030794fee2d909697ad967129d8d8a79
wohyperion <whisperofhyperion@gmail.com>
2019-06-13 03:21:47 +0700

    Movie by genres ListView added +new url for this +new template that extends movie_list.html template

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
     path('', views.MovieListView.as_view(), name='index'),
-    path('<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail')
+    path('<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
+    path('<genre>/', views.GenreMovieListView.as_view(), name='genre-detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 
-from .models import (Movie)
+from .models import (Movie, Genre)
 
 
 # Create your views here.
@@ -12,7 +12,28 @@ def index(request):
 
 class MovieListView(ListView):
     model = Movie
-    paginate_by = 12
+    paginate_by = 24
+
+    def get_context_data(self, **kwargs):
+        context = super(MovieListView, self).get_context_data()
+        context['genre_list'] = Genre.objects.all()
+        return context
+
+
+class GenreMovieListView(ListView):
+    model = Movie
+    paginate_by = 24
+    template_name = 'movies/movie_by_genre.html'
+
+    def get_queryset(self):
+        genre = get_object_or_404(Genre, title=self.kwargs['genre'])
+        return Movie.objects.filter(genre=genre)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(GenreMovieListView, self).get_context_data()
+        context['genre_list'] = Genre.objects.all()
+        context['genre'] = get_object_or_404(Genre, title=self.kwargs['genre'])
+        return context
 
 
 class MovieDetailView(DetailView):
