e8565ad03ff4fbd44e9fdce486f80191edca88c2
wohyperion <whisperofhyperion@gmail.com>
2019-06-12 19:08:45 +0700

    Movie list and details has been created Added urls for this

---
+++ movies/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'movies'
+urlpatterns = [
+    path('', views.MovieListView.as_view(), name='index'),
+    path('<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail')
+]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import (Movie)
+
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'base.html', context)
+
+
+class MovieListView(ListView):
+    model = Movie
+    paginate_by = 12
+
+
+class MovieDetailView(DetailView):
+    model = Movie
