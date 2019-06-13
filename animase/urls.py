ad9714a8897148cfcd48f7c45b5106fe78d579a6
wohyperion <whisperofhyperion@gmail.com>
2019-06-13 19:09:24 +0700

    Create login and logout pages

--- animase/urls.py
+++ animase/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/movies/', permanent=True)),
     path('movies/', include('movies.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
