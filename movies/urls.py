4dec7732c1257002510ba8b047f9c4020ec1adc2
wohyperion <whisperofhyperion@gmail.com>
2019-06-18 01:37:06 +0700

    Views improved and new created MovieByGenreList: ordering added Created: MovieCreateView, MovieUpdateView, GenreModalView, GenreListView, StudioModalView URLs updated

--- movies/urls.py
+++ movies/urls.py
@@ -5,8 +5,13 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
     path('', views.MovieListView.as_view(), name='index'),
+    path('create/', views.MovieCreateView.as_view(), name='movie-create'),
+    path('<int:pk>/edit/', views.MovieUpdateView.as_view(), name='movie-update'),
     path('<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
     path('<int:pk>/favorite/', views.FavoriteMovieView.as_view(), name='movie-favorite'),
     path('scores/', views.ScoreListView.as_view(), name='score-list'),
+    path('genres/', views.GenreListView.as_view(), name='genre-list'),
+    path('genres/create/', views.GenreModalView.as_view(), name='genre-create'),
+    path('studios/create/', views.StudioModalView.as_view(), name='studio-create'),
     path('<genre>/', views.MovieByGenreListView.as_view(), name='genre-detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView, View
-from django.http import HttpResponse, HttpResponseForbidden
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.core.serializers import serialize
 
-from .models import (Movie, Genre, Score, Favorite)
+from .models import (Movie, Genre, Score, Favorite, Studio)
+from .forms import (MovieForm)
 
 
 class MovieListView(ListView):
@@ -35,6 +40,7 @@ class MovieByGenreListView(ListView):
     model = Movie
     paginate_by = 24
     template_name = 'movies/movie_by_genre.html'
+    ordering = ['-added_date']
 
     def get_queryset(self):
         genre = get_object_or_404(Genre, title=self.kwargs['genre'])
@@ -45,6 +51,73 @@ class MovieByGenreListView(ListView):
         context['genre_list'] = Genre.objects.all()
         context['genre'] = get_object_or_404(Genre, title=self.kwargs['genre'])
         return context
+
+
+class MovieCreateView(LoginRequiredMixin, CreateView):
+    model = Movie
+    login_url = reverse_lazy('login')
+    form_class = MovieForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(MovieCreateView, self).get_context_data()
+        context['form']['studio'].field.empty_label = None
+        return context
+
+
+class MovieUpdateView(LoginRequiredMixin, UpdateView):
+    model = Movie
+    login_url = reverse_lazy('login')
+    form_class = MovieForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['form']['studio'].field.empty_label = None
+
+        movie = Movie.objects.get(pk=self.kwargs['pk'])
+        context['current'] = {
+            'type': movie.type,
+            'status': movie.status,
+            'source': movie.source,
+            'rating': movie.rating,
+            'studio': movie.studio_id,
+            'genres': movie.genre.all().values_list('pk', flat=True),
+            'related': movie.related.all().values_list('pk', flat=True),
+        }
+
+        return context
+
+
+class GenreModalView(View):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated and not request.is_ajax:
+            return HttpResponseForbidden()
+        else:
+            obj, created = Genre.objects.get_or_create(title=request.POST.get('title'))
+
+        if created:
+            return HttpResponse(f'{obj.pk},{obj.title}')
+
+        return HttpResponse(created)
+
+
+# TODO: delete if dont need it and clean urls
+class GenreListView(View):
+    http_method_names = ['get']
+
+    def get(self, request):
+        queryset = Genre.objects.all().only('title')
+        data = serialize('json', Genre.objects.all(), fields='title')
+        return JsonResponse(data, safe=False)
 
 
 class ScoreListView(ListView):
@@ -70,3 +143,18 @@ class FavoriteMovieView(View):
                 obj.delete()
 
             return HttpResponse(created)
+
+
+class StudioModalView(View):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated and not request.is_ajax:
+            return HttpResponseForbidden()
+        else:
+            obj, created = Studio.objects.get_or_create(name=request.POST.get('name'))
+
+        if created:
+            return HttpResponse(f'{obj.pk},{obj.name}')
+
+        return HttpResponse(created)
