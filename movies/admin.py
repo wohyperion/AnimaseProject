eeca478e8b529e8d996fef64988dee39627180ca
wohyperion <whisperofhyperion@gmail.com>
2019-06-25 20:42:24 +0700

    Added Permission section to admin page

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
 
 from . import models
+
+admin.site.register(Permission)
 
 admin.site.register(models.Genre)
 admin.site.register(models.Studio)
